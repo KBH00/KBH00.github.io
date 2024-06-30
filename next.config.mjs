37e234f2f2efd59d61daa2d2f35606806090dd09
KBH00 <wlfjddlgoqn@knu.ac.kr>
2024-06-30 22:54:03 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,14 +9,11 @@ const nextConfig = {
    *
    */
   output: "export",
-  reactStrictMode: true,
   assetPrefix: prefix,
   /**
    * Set base path. This is usually the slug of your repository.
    *
    */
-  basePath: "/kbh00.github.io",
-
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
